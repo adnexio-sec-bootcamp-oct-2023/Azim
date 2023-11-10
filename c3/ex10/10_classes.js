9e413697ea1861feaa39868d58afe3d06312c2cf
Azim Azidin <azimazid@gmail.com>
2023-11-10 15:40:50 +0800

    Exercise 10

--- c3/ex10/10_classes.js
+++ c3/ex10/10_classes.js
@@ -1,7 +1,58 @@
 /*
 Design a Vehicle class with properties like make, model, and year. Implement methods to:
-Display vehicle details using a template literal.
-A static method that returns the number of vehicle instances created.
-Then, extend this Vehicle class with a Car subclass. The Car class should have additional properties 
-like mileage and a method to calculate the age of the car based on the current year.
+1. Display vehicle details using a template literal.
+2. A static method that returns the number of vehicle instances created.
+Then, extend this Vehicle class with a Car subclass. The Car class should have 
+additional properties like mileage and a method to calculate the age of the car 
+based on the current year.
 */
+
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        Vehicle.incrementInstanceCount();
+    }
+
+    displayDetails() {
+        return `Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`;
+    }
+
+    static incrementInstanceCount() {
+        if (!this.instanceCount) {
+            this.instanceCount = 0;
+            console.log(this.instanceCount);
+        }
+        this.instanceCount++;
+        console.log(this.instanceCount);
+    }
+
+    static getInstanceCount() {
+        return this.instanceCount || 0;
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year, mileage) {
+        super(make, model, year);
+        this.mileage = mileage;
+    }
+
+    calculateAge(currentYear) {
+        return currentYear - this.year;
+    }
+}
+
+const vehicle1 = new Vehicle('Toyota', 'Vios', 2023);
+const vehicle2 = new Vehicle('Honda', 'CRV', 2007);
+const car1 = new Car('Perodua', 'Kancil', 1999, 310000);
+
+console.log(vehicle1.displayDetails());
+console.log(vehicle2.displayDetails());
+console.log(car1.displayDetails());
+
+const currentYear = new Date().getFullYear();
+console.log(`Car1 Age: ${car1.calculateAge(currentYear)}`);
+
+console.log(`Number of vehicle instances created: ${Vehicle.getInstanceCount()}`);
--- c3/ex9/9_this.js
+++ c3/ex9/9_this.js
@@ -1,3 +1,49 @@
 /*Develop a function within an object that returns the object's properties using the this keyword. 
 Call the function in different contexts (e.g., globally, within the object, using call/apply/bind) 
 to observe changes in the value of this.*/
+
+const myObject = {
+    name: 'Jungkook',
+    age: 29,
+    city: 'Seoul',
+    getProperties: function() {
+      return {
+        name: this.name,
+        age: this.age,
+        city: this.city,
+        //name: myObject.name,
+        //age: myObject.age,
+        //city: myObject.city,
+      };
+    },
+  };
+  
+  console.log('Within object:');
+  console.log(myObject.getProperties());
+  
+  const globalFunction = myObject.getProperties;
+  console.log('\nGlobally:');
+  console.log(globalFunction());
+  // this just creates a reference to getProperties without its original
+  // context. No access to myObject object. Need to use call/apply/bind
+  // for globalfunction to work 
+  
+  const anotherObject = {
+    name: 'Ali',
+    age: 25,
+    city: 'Los Seremban',
+  };
+  
+  console.log('\nUsing call:');
+  console.log(globalFunction.call(myObject));
+
+  console.log('\nUsing call:');
+  console.log(myObject.getProperties.call(anotherObject));
+  
+  console.log('\nUsing apply:');
+  console.log(myObject.getProperties.apply(anotherObject));
+  
+  const boundFunction = myObject.getProperties.bind(anotherObject);
+  console.log('\nUsing bind:');
+  console.log(boundFunction());
+  
